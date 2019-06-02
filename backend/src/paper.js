58b9891c3353b7373febb052217994eac12ac7dd
sihang.li <lisihang@pku.edu.cn>
2019-06-02 18:28:02 +0800

    Revert "md to pdf"
    This reverts commit c642456b0e4ec89762b5e463087a35328f383ba3.

--- backend/src/paper.js
+++ backend/src/paper.js
@@ -4,12 +4,31 @@ var Question = models.question;
 var Paper = models.paper;
 
 var fs = require("fs");
-var markdownpdf = require("markdown-pdf")
 
 var Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
+var gulp = require("gulp");
+var markdown = require('gulp-markdown');
+var mdpdf = require('gulp-markdown-pdf');
+var watch = require("gulp-watch");
+
 var root = "C:/"
+
+gulp.task("compileMDToPdf", function () {
+    gulp.src(root + "cache/md/*.md")
+        .pipe(mdpdf())
+        .pipe(gulp.dest(root + "cache/pdf"))
+    console.log("hhh");
+})
+
+gulp.task("watch", function() {
+    console.log("hhh");
+    watch(root + "cache/md/*.md", gulp.series('compileMDToPdf'));
+    //watch(root + "cache/md/*.md", ['compileMDToPdf'])
+})
+
+gulp.task("default", gulp.series('watch'))
 
 var paper = 
 {
@@ -24,8 +43,6 @@ var paper =
             order: ['type']
         }).then(function(questions)
         {
-            for (var i in questions)
-                questions[i].increment('hot');
             var s = "";
             for (var i in ids)
                 s += ids[i].toString() + " ";
@@ -45,23 +62,19 @@ var paper =
                     if (exists)
                         return pdfname;
                     var writeData = "";
-                    writeData += "# " + title + "\n\n";
+                    writeData += "### " + title + "\n\n";
+                    writeData += "##### Made by PaperMaker\n\n";
                     writeData += "------\n\n";
                     for (var i in questions)
                     {
                         if (i == 0 || questions[i].type != questions[i - 1].type)
-                            writeData += "\n### " + questions[i].type + "\n\n";
+                            writeData += "\n##### " + questions[i].type + "\n\n";
                         writeData += (i + 1).toString() + ". " + questions[i].content + "\n\n";
                     }
                     fs.writeFile(mdname, writeData, {flag: 'a'}, function(err)
                     {
                         if (err == null)
-                        {
-                            markdownpdf({'cssPath': 'github-markdown.css'}).from(mdname).to(pdfname, function()
-                            {
-                                res.download(pdfname, "paper.pdf");
-                            });
-                        }
+                            res.download(mdname, "paper.md");
                     });
                 });
             });
