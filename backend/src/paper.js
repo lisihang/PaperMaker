88e21bb83726412520412320bf1d289d44e6b444
sihang.li <lisihang@pku.edu.cn>
2019-06-06 09:24:45 +0800

    paper sort

--- backend/src/paper.js
+++ backend/src/paper.js
@@ -50,6 +50,26 @@ var paper =
                     writeData += "------\n\n";
                     for (var i in questions)
                     {
+                        if (questions[i].type == "选择题")
+                            questions[i].rank = 1;
+                        else if (questions[i].type == "填空题")
+                            questions[i].rank = 2;
+                        else if (questions[i].type == "简答题")
+                            questions[i].rank = 3;
+                        else
+                            questions[i].rank = 4;
+                    }
+                    questions.sort((a, b)=>
+                    {
+                        if (a.rank != b.rank)
+                            return a.rank - b.rank;
+                        else if (a.type != b.type)
+                            return a.type.localeCompare(b.type);
+                        else
+                            return a.id - b.id;
+                    });
+                    for (var i in questions)
+                    {
                         if (i == 0 || questions[i].type != questions[i - 1].type)
                             writeData += "\n### " + questions[i].type + "\n\n";
                         writeData += (parseInt(i) + 1).toString() + ". " + questions[i].content + "\n\n";
@@ -94,6 +114,26 @@ var paper =
                 var writeData = "";
                 writeData += "# " + title + " 参考答案\n\n";
                 writeData += "------\n\n";
+                for (var i in questions)
+                {
+                    if (questions[i].type == "选择题")
+                        questions[i].rank = 1;
+                    else if (questions[i].type == "填空题")
+                        questions[i].rank = 2;
+                    else if (questions[i].type == "简答题")
+                        questions[i].rank = 3;
+                    else
+                        questions[i].rank = 4;
+                }
+                questions.sort((a, b)=>
+                {
+                    if (a.rank != b.rank)
+                        return a.rank - b.rank;
+                    else if (a.type != b.type)
+                        return a.type.localeCompare(b.type);
+                    else
+                        return a.id - b.id;
+                });
                 for (var i in questions)
                 {
                     if (i == 0 || questions[i].type != questions[i - 1].type)
